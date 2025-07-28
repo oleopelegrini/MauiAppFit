356c073eb892a3e798963a55f0dd507942ed947d
231284 <231284@CPD03-M07>
2025-07-28 11:40:09 -0300

    databasehelper

--- MauiAppFit/Helpers/SQLiteDataBaseHelper.cs
+++ MauiAppFit/Helpers/SQLiteDataBaseHelper.cs
@@ -1,12 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MauiAppFit.Models;
+using SQLite;
 
 namespace MauiAppFit.Helpers
 {
-    internal class SQLiteDataBaseHelper
+    public class SQLiteDataBaseHelper
     {
+        readonly SQLiteConnection _db;
+
+        public SQLiteDataBaseHelper(string dbPath)
+        {
+            _db = new SQLiteAsyncConnection(dbPath);
+            _db.CreateTableAsync<Atividade>().Wait();
+        }
+
+        public Task<List<Atividade>> GetAllRows()
+        {
+            return _db.Table<Atividade>().OrderByDescending(i => i.Id).ToListAsync();
+        }
+        
+        public Task<Atividade> GetById(int id)
+        {
+            return _db.Table<Atividade>().FirstAsync(i => i.Id == id);
+        }
     }
 }
