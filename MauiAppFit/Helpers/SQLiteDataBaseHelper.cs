67c9330a1133e63e182978b7191d8ad659acecab
231284 <231284@CPD03-M07>
2025-07-28 14:36:18 -0300

    Fechando SQLiteDataBaseHelper

--- MauiAppFit/Helpers/SQLiteDataBaseHelper.cs
+++ MauiAppFit/Helpers/SQLiteDataBaseHelper.cs
@@ -5,7 +5,7 @@ namespace MauiAppFit.Helpers
 {
     public class SQLiteDataBaseHelper
     {
-        readonly SQLiteConnection _db;
+        readonly SQLiteAsyncConnection _db;
 
         public SQLiteDataBaseHelper(string dbPath)
         {
@@ -21,6 +21,26 @@ namespace MauiAppFit.Helpers
         public Task<Atividade> GetById(int id)
         {
             return _db.Table<Atividade>().FirstAsync(i => i.Id == id);
+        }
+        
+        public Task<int> Insert(Atividade model)
+        {
+            return _db.InsertAsync(model);
+        }
+
+        public Task<List<Atividade>> Update(Atividade model)
+        {
+            string sql = "UPDATE Atividade SET Descricao=?, Data=?, Peso=?, " +
+                "Observacoes=? WHERE Id=?";
+
+            return _db.QueryAsync<Atividade>(
+                sql,
+                model.Descricao,
+                model.Data,
+                model.Peso,
+                model.Observacoes,
+                model.Id
+            );
         }
     }
 }
